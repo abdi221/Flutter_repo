e9f9b4fa293ccf87c439e0f7840449f07341bbf7
Abdifataah Abdillahi Ali <abdifatahali22@gmail.com>
2025-06-24 21:11:18 +0200

    Cleaned most of the code base

--- flutterapp/lib/main.dart
+++ flutterapp/lib/main.dart
@@ -15,21 +15,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 234, 3, 3)),
       ),
       home: const MyHomePage(title: 'Personal Card'),
@@ -40,135 +25,99 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
-
 class _MyHomePageState extends State<MyHomePage> {
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
           // wireframe for each widget.
           
           mainAxisAlignment: MainAxisAlignment.center,
             children: [
                 CircleAvatar(
-                radius: 74,
+                radius: 90,
                 backgroundImage: AssetImage('assets/images/landskape.jpeg'),
               ),
-              const SizedBox(height: 16,),
+              const SizedBox(height: 32),
+              SizedBox(height: 32),
+
               Text('Abdifataah Abdillahi Ali', 
               style: GoogleFonts.dancingScript(fontSize: 28,
                fontWeight: FontWeight.bold)
               
                ),
-              Text('Student at Linnaeus University',
-              style: TextStyle(fontWeight: FontWeight.bold),
-              ),
-              Row(
-                children: const [
-                  
-                  Icon(Icons.email),
-                  SizedBox(width: 12),
-                  Text(
-                    'E-mail: aa226wu@student.lnu.se', 
-                    style:TextStyle(fontSize: 12)
-                  )
-                ]
-              ),
-              Row(
-                children: [
-                  Icon(Icons.phone),
-                  SizedBox(width: 12),
-                  Text(
-                    'Phone: 0701234567',
-                    style:TextStyle(fontSize: 12)
-                    )
-                ],
-                ),
-                Row(
-                  children: [
-                  Icon(Icons.web),
-                  SizedBox(width: 12),
-                  Text(
-                    'Website: https://lnu.se/ ',
-                    style:TextStyle(fontSize: 12)
-                    )
-                  ]
-                )
+               SizedBox(height: 102),
 
-              
+              Card(
+                color: const Color.fromARGB(255, 240, 107, 107),
+                elevation: 5,
+                margin: EdgeInsets.symmetric(horizontal: 20,
+                vertical: 16),
+                child: Padding(padding: EdgeInsets.symmetric(
+                  horizontal: 16,
+                  vertical: 20),
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('Student at Linnaeus University',
+                      style: TextStyle(fontWeight: FontWeight.bold, fontSize: 12),
+                      ),
+
+                      SizedBox(height: 16),
+                    
+                      Row(
+                        children: const [
+                          
+                          Icon(Icons.email),
+                          SizedBox(width: 12),
+                          Text(
+                            'Email: aa226wu@student.lnu.se', 
+                            style:TextStyle(fontSize: 16, fontWeight: FontWeight.normal)
+                          )
+                        ]
+                      ),
+                      Row(
+                        children: [
+                          Icon(Icons.phone),
+                          SizedBox(width: 12),
+                          Text(
+                            'Phone: 0701234567',
+                            style:TextStyle(fontSize: 16, fontWeight: FontWeight.normal)
+                            )
+                        ],
+                        ),
+                        Row(
+                          children: [
+                          Icon(Icons.web),
+                          SizedBox(width: 12),
+                          Text(
+                            'Website: https://lnu.se/ ',
+                            style:TextStyle(fontSize: 16, fontWeight: FontWeight.normal)
+                            )
+                          ]
+                        )
+
+
+                    ],
+                  )
+                )),
 
               ]
 
            )
-           //<Widget>[
-          //   const Text('You have pushed the button this many times:'),
-          //   Text(
-          //     '$_counter',
-          //     style: Theme.of(context).textTheme.headlineMedium,
-          //   ),
-          // ],
         ),
       
-      // floatingActionButton: FloatingActionButton(
-      //   onPressed: _incrementCounter,
-      //   tooltip: 'Increment',
-      //   child: const Icon(Icons.add),
-      // ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
