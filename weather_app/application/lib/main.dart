dd59884b37ffa1e45678134bd063e4ca9829b169
abdi221 <abdifatahali22@gmail.com>
2025-08-05 11:22:09 +0200

    Change name and config main.dart accordingly

--- weather_app/application/lib/main.dart
+++ weather_app/application/lib/main.dart
@@ -1,20 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: '.env');
+
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
       home: Scaffold(
         body: Center(
           child: Text("Hello world"),
-          )
+        ),
       ),
     );
   }
